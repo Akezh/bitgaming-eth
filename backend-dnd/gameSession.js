df023f225938f09e4fd7985a1d601dc7368bf97c
Akezhan Rakishev <rakishev.akezhan@gmail.com>
2024-10-20 07:26:32 -0700

    feat: merge all the work

---
+++ backend-dnd/gameSession.js
@@ -0,0 +1,130 @@
+require("dotenv").config();
+const OpenAI = require("openai");
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+
+// const userSessionDictionary = new Map<string, { assistantId: string; sessions: Record<string, string> }>();
+const userSessionDictionary = new Map();
+
+function generate_instruction(gameSettings) {
+  const instructions = `You are the author of an interactive quest in a ${gameSettings.genre} setting. 
+    Come up with an interesting story. Your message is a part of the story that forces the player(s) to make a choice.
+    The game should consist of a short part (up to ${gameSettings.max_chars} characters) of your story and the options for player actions you propose.
+    At the end of each of your messages, ask a question about how the player should act in the current situation, except the last turn. 
+    Offer at minimum three options to choose from, but leave the opportunity to offer actions by player, the options should start with "1.", "2.", etc..
+    The quest must be completed within ${gameSettings.turns} player turns.
+    The game can be played by only one player. 
+    Create a story. Players will respond with the structure {"user": "Response"}.
+    With each turn the situation should become more intense and logically related to the previous turn.
+    The player may encounter various dangers on theirs journey. 
+    If the player chooses a bad answer, player may die and then the game will end.
+    Use a speaking style that suits the chosen setting.
+    Each time you would be notified with the current turn/round number.
+        Make sure to finish the story within ${gameSettings.turns} rounds.
+        Don't ask the user anything after the game finishes. Just congratulate.
+    Communicate with players in (${gameSettings.language} language). Each response should be in the same language - ${gameSettings.language}.
+    After the end of the game (due to the death of the player or due to the fact that all turns have ended), invite the player to start again`
+  return instructions;
+}
+
+const startGameSession = async (
+  wallet_address,
+  session_id,
+  gameSettings,
+) => {
+    if (!userSessionDictionary.has(wallet_address)) {
+      const assistant = await openai.beta.assistants.create({
+        name: 'Roleplaying game master',
+        instructions: generate_instruction(gameSettings),
+        model: 'gpt-4o',
+      });
+
+      // for each separate sessions, separate threads are created
+      userSessionDictionary.set(wallet_address, { assistantId: assistant.id, sessions: {} });
+    }
+    
+    const assistantId = userSessionDictionary.get(wallet_address)?.assistantId;
+    const thread = await openai.beta.threads.create();
+
+    // @ts-ignore
+    userSessionDictionary.get(wallet_address).sessions[session_id] = thread.id;
+    
+    try {
+      const run = await openai.beta.threads.runs.create(thread.id, { assistant_id: assistantId });
+      
+      const ai_message = await new Promise(resolve => {
+        const timerLoopId = setInterval(async () => {
+          const run_data = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+          if (run_data.status === 'completed') {
+            clearInterval(timerLoopId);
+            const messages = await openai.beta.threads.messages.list(thread.id);
+            const ai_response = messages.data[0].content[0];
+            resolve(ai_response.text.value);
+          }
+          if (run_data.status === 'failed' || run_data.status === 'expired' || run_data.status === 'cancelled') {
+            clearInterval(timerLoopId);
+            resolve('Sorry, I am having trouble understanding you. Could you please rephrase your question?');
+          }
+        }, 2000);
+      });
+
+      return ai_message;
+    } catch (error) {
+      console.error('Error fetching data from OpenAI:', error);
+    }
+};
+
+const sendQuery = async (
+  wallet_address,
+  session_id,
+  userPrompt) => {
+    const assistantId = userSessionDictionary.get(wallet_address)?.assistantId;
+    const threadId = userSessionDictionary.get(wallet_address)?.sessions[session_id];
+    // console.log(userSessionDictionary, threadId)
+    
+    console.log(userPrompt)
+    await openai.beta.threads.messages.create(threadId, {
+      role: 'user',
+      content: userPrompt,
+    });
+    
+    try {
+      const run = await openai.beta.threads.runs.create(threadId, { assistant_id: assistantId });
+      
+      const ai_message = await new Promise(resolve => {
+        const timerLoopId = setInterval(async () => {
+          const run_data = await openai.beta.threads.runs.retrieve(threadId, run.id);
+          if (run_data.status === 'completed') {
+            clearInterval(timerLoopId);
+            const messages = await openai.beta.threads.messages.list(threadId);
+            const ai_response = messages.data[0].content[0];
+            resolve(ai_response.text.value);
+          }
+          if (run_data.status === 'failed' || run_data.status === 'expired' || run_data.status === 'cancelled') {
+            clearInterval(timerLoopId);
+            resolve('Sorry, I am having trouble understanding you. Could you please rephrase your question?');
+          }
+        }, 2000);
+      });
+
+      return ai_message;
+    } catch (error) {
+      console.error('Error fetching data from OpenAI:', error);
+    }
+}
+
+async function generateImageResponse(prompt) {
+  const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+  const response = await openai.images.generate({
+    model: "dall-e-3",
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  return response;
+}
+
+module.exports = {
+  startGameSession,
+  generateImageResponse,
+  sendQuery
+};
---
+++ backend-dnd/server.js
@@ -0,0 +1,187 @@
+require("dotenv").config(); // Load environment variables
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const { startGameSession, sendQuery, generateImageResponse } = require("./gameSession");
+const {
+  LANGUAGES,
+  GENRES,
+  MAX_TURNS,
+  gameInstructions,
+  extractOptionsFromAIResponse,
+} = require("./utils");
+const { v4: uuidv4 } = require('uuid');
+
+const uuid = uuidv4();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const fs = require("fs").promises;
+const axios = require("axios");
+const json = require("body-parser/lib/types/json");
+
+// Store game settings for active sessions
+let gameSettings = {};
+
+// Serve static files from the "public" directory (your frontend)
+app.use(express.static("public"));
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
+
+// Use body-parser middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Route to serve the main HTML page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/web_index.html");
+});
+
+// POST route to start a new game session
+app.post("/start", async (req, res) => {
+  console.log(req.body);
+
+  const { gptVersion, language, genre, turns, wallet_address } = req.body;
+
+  // Initialize game settings
+  gameSettings = { gptVersion, language, genre, turns, round: 1, isGameStarted: true };
+
+  // Start the game and get the initial response
+  const response = await startGameSession(wallet_address, uuid, gameSettings);
+  console.log(response)
+  const choices = extractOptionsFromAIResponse(response);
+
+  res.json({ message: response, choices });
+});
+
+// POST route to continue the game with user input
+app.post("/continue", async (req, res) => {
+  console.log(req.body);
+  const { userPrompt, wallet_address } = req.body;
+
+  if (!gameSettings.isGameStarted) {
+    return res.json({ message: "No active game session. Start a new game!" });
+  }
+
+  // Increment the round and check if the game should end
+  gameSettings.round++;
+  if (gameSettings.round > +gameSettings.turns) {
+    gameSettings.isGameStarted = false;
+    return res.json({ message: "Game finished. Start a new game?" });
+  }
+
+  // Continue the game session with the user's input
+  const response = await sendQuery(wallet_address, uuid, userPrompt);
+  const choices = extractOptionsFromAIResponse(response);
+
+  // Generate an image related to the game response
+  const imageResponse = await generateImageResponse(response);
+
+  res.json({ message: response, choices, imageUrl: imageResponse.data[0].url });
+  const walrus_data = await uploadToWalrus(imageResponse.data[0].url);
+  updateJsonFile(walrus_data);
+
+});
+
+app.get("/gallery", async (req, res) => {
+  const walrus_json = "saved_files/walrus_blob_id.json"
+  const fileContent = await fs.readFile(walrus_json, 'utf8');
+  jsonData = JSON.parse(fileContent);
+  res.json(jsonData);
+
+})
+
+function generateUniqueFilename() {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  return `dnd_${timestamp}.png`;
+}
+
+async function uploadToWalrus(imageUrl) {
+    try {
+      // Fetch the image from the URL and convert it to a Blob
+
+      const basePublisherUrl = "https://walrus-testnet-publisher.nodes.guru";
+      const numEpochs = 1;
+
+      const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+  
+      if (!(response.status === 200)) {
+        throw new Error("Failed to fetch the image from URL");
+      }
+
+      // Submit a PUT request to store the image blob to Walrus
+      const walrusResponse = await fetch(`${basePublisherUrl}/v1/store?epochs=${numEpochs}`, {
+        method: "PUT",
+        body: response.data,
+      });
+  
+      if (walrusResponse.status === 200) {
+        // Parse successful responses as JSON, and return the blob ID along with the mime type
+        const info = await walrusResponse.json();
+        console.log("Stored blob info:", info);
+        if ("alreadyCertified" in info) {
+          storage_info = {
+            status: "Already certified",
+            blobId: info.alreadyCertified.blobId,
+            endEpoch: info.alreadyCertified.endEpoch,
+          };
+        } else {
+          storage_info = {
+            status: "Newly created",
+            blobId: info.newlyCreated.blobObject.blobId,
+            endEpoch: info.newlyCreated.blobObject.storage.endEpoch,
+          };
+        }
+
+
+        return { blob_id: storage_info.blobId, endEpoch: storage_info.endEpoch };
+      } else {
+        console.log(walrusResponse)
+        throw new Error("Something went wrong when storing the blob to Walrus!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  async function updateJsonFile(newData) {
+    try {
+      // Read the existing data from the JSON file (if it exists)
+      let jsonData = {};
+      const walrus_json = "saved_files/walrus_blob_id.json"
+
+      try {
+        const fileContent = await fs.readFile(walrus_json, 'utf8');
+        jsonData = JSON.parse(fileContent); // Parse the JSON content into an object
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          // If the file does not exist, create the file with an empty object
+          console.log("File not found, creating a new one...");
+          jsonData = {}; // Initialize empty object
+          await fs.writeFile(walrus_json, JSON.stringify(jsonData, null, 2), 'utf8'); // Create an empty JSON file
+        } else {
+          // Re-throw other errors
+          throw err;
+        }
+      }
+  
+      // Update the data with the new entry (you can customize the update logic)
+      // Assuming newData contains blob_id and media_type
+      jsonData[generateUniqueFilename()] = newData;
+  
+      // Write the updated data back to the file
+      await fs.writeFile(walrus_json, JSON.stringify(jsonData, null, 2), 'utf8');
+  
+      console.log('JSON file updated successfully!');
+    } catch (error) {
+      console.error('Error updating JSON file:', error);
+    }
+  }
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
